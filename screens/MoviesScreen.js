96b1be8258fab04a4c9eb3e8677d40711c599c17
chenlujia <lujia.chen@aihuishou.com>
2019-03-29 11:03:36 +0800

    init

---
+++ screens/MoviesScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+export default class MoviesScreen extends Component {
+  render() {
+    return (
+     <View>
+       <Text>First RN Project</Text>
+     </View>
+    )
+  }
+}
